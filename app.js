a49a0666695f0c80d89e9e321ce4e1c5589049a4
Ben Phelps <bphelpsen@gmail.com>
2015-11-12 07:38:12 +0000

    test7

--- app.js
+++ app.js
@@ -73,6 +73,6 @@ app.get(/(.*)/, function (req, res) {
 
 var server = app.listen(process.env.PORT, function () {
   var host = process.env.IP;
-  var port = process.env.port;
+  var port = process.env.PORT;
   console.log('Example app listening at http://%s:%s', host, port);
 });
